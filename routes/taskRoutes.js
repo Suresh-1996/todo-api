a857a96bd9366efa4cb9ec237e61eb8fb167e18b
Suresh-1996 <sureshmadhushanka11@gmail.com>
2025-01-11 07:41:45 +0530

    preloder added

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,43 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models/db');
+const db = require("../models/db");
 
 // Get all tasks
-router.get('/', (req, res) => {
-    db.query('SELECT * FROM tasks', (err, results) => {
-        if (err) return res.status(500).send(err);
-        res.json(results);
-    });
+router.get("/", (req, res) => {
+  db.query("SELECT * FROM tasks", (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
 });
 
 // Add a task
-router.post('/', (req, res) => {
-    const { name, description } = req.body;
-    const query = 'INSERT INTO tasks (name, description) VALUES (?, ?)';
-    db.query(query, [name, description], (err) => {
-        if (err) return res.status(500).send(err);
-        res.send('Task added!');
-    });
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+  const query = "INSERT INTO tasks (name, description) VALUES (?, ?)";
+  db.query(query, [name, description], (err) => {
+    if (err) return res.status(500).send(err);
+    res.send("Task added!");
+  });
 });
 
 // Edit a task
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, description, status } = req.body;
-    const query = 'UPDATE tasks SET name = ?, description = ?, status = ? WHERE id = ?';
-    db.query(query, [name, description, status, id], (err) => {
-        if (err) return res.status(500).send(err);
-        res.send('Task updated!');
-    });
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, description, status } = req.body;
+  const query =
+    "UPDATE tasks SET name = ?, description = ?, status = ? WHERE id = ?";
+  db.query(query, [name, description, status, id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.send("Task updated!");
+  });
 });
 
 // Delete a task
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    db.query('DELETE FROM tasks WHERE id = ?', [id], (err) => {
-        if (err) return res.status(500).send(err);
-        res.send('Task deleted!');
-    });
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db.query("DELETE FROM tasks WHERE id = ?", [id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.send("Task deleted!");
+  });
 });
 
 module.exports = router;
